1b64126b19fe8c170c95fffd3b468e88dcc57cfc
Henrique1803 <henriquemateusteodoro@gmail.com>
2023-12-03 15:21:05 -0300

    main

--- versao_final/Main.py
+++ versao_final/Main.py
@@ -3,6 +3,7 @@ import sys
 from estados.State import GerenciadoraDeEstados
 
 
+#caso precise colocar um caminho próprio
 #sys.path.append(r'c:\atividades\ProjetoFinal\projeto-final-grupo-3-23-2\versao_final')
 pygame.init()
 jogo = GerenciadoraDeEstados()
